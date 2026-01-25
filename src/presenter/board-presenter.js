05f4ac6e83e406f1ed66a1b1aeb4f8a51747e713
Yana <holoimenko.yana@yandex.ru>
2026-01-25 07:39:46 +0700

    fix: работа с замечаниями

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,4 +1,3 @@
-import BoardView from '../view/board-view.js';
 import SortView from '../view/sort-view.js';
 import PointListView from '../view/point-list-view.js';
 import AddNewPointView from '../view/add-new-point-view.js';
@@ -7,22 +6,22 @@ import PointView from '../view/point-view.js';
 
 import {render} from '../render.js';
 
+const INITIAL_POINTS_COUNT = 3;
+
 export default class BoardPresenter {
-  boardComponent = new BoardView();
-  pointListComponent = new PointListView();
 
   constructor({boardContainer}) {
     this.boardContainer = boardContainer;
+    this.pointListComponent = new PointListView();
   }
 
   init() {
-    render(this.boardComponent, this.boardContainer);
-    render(new SortView(), this.boardComponent.getElement());
-    render(this.pointListComponent, this.boardComponent.getElement());
+    render(new SortView(), this.boardContainer);
+    render(this.pointListComponent, this.boardContainer);
     render(new EditPointView(), this.pointListComponent.getElement());
     render(new AddNewPointView(), this.pointListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < INITIAL_POINTS_COUNT; i++) {
       render(new PointView(), this.pointListComponent.getElement());
     }
   }
